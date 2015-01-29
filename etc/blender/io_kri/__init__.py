2cdb89c2adf8cd4e54652a4c7a5ae6bb19892434
Dzmitry Malyshau <kvarkus@gmail.com>
2015-01-29 00:48:01 -0500

    KRI Exporter is ported, using JSON output

---
+++ etc/blender/io_kri/__init__.py
@@ -0,0 +1,8 @@
+bl_info = {
+    'name': 'KRI common routines',
+    'author': 'Dzmitry Malyshau',
+    'version': (0, 1, 0),
+    'blender': (2, 6, 2),
+    'warning': '',
+    'category': 'Import-Export'
+}
