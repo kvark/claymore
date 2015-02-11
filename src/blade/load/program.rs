e8c6186c6ce5cda9f187f9520fc23cab0f18e1ac
Dzmitry Malyshau <kvarkus@gmail.com>
2015-02-11 00:22:45 -0500

    Implemented a simple Lambertian diffuse for the shader

--- src/blade/load/program.rs
+++ src/blade/load/program.rs
@@ -5,15 +5,17 @@ use super::scene::Scalar;
 pub static VERTEX_SRC: ShaderSource<'static> = shaders! {
     glsl_150: b"#version 150 core
 
-    in vec3 a_Position;
-    //in vec2 a_TexCoord;
-    //out vec2 v_TexCoord;
-
     uniform mat4 u_Transform;
+    uniform mat3 u_NormalRotation;
+
+    in vec3 a_Position;
+    in vec3 a_Normal;
+
+    out vec3 v_Normal;
 
     void main() {
-        //v_TexCoord = a_TexCoord;
         gl_Position = u_Transform * vec4(a_Position, 1.0);
+        v_Normal = u_NormalRotation * a_Normal;
     }
     "
 };
@@ -21,13 +23,16 @@ pub static VERTEX_SRC: ShaderSource<'static> = shaders! {
 pub static FRAGMENT_SRC: ShaderSource<'static> = shaders! {
     glsl_150: b"#version 150 core
 
-    //in vec2 v_TexCoord;
-    out vec4 o_Color;
-
+    const vec3 c_LightPos = vec3(10.0, 10.0, 10.0); //view space
     uniform vec4 u_Color;
 
+    in vec3 v_Normal;
+    out vec4 o_Color;
+
     void main() {
-        o_Color = u_Color;
+        vec3 N = normalize(v_Normal);
+        vec3 L = normalize(c_LightPos);
+        o_Color = u_Color * dot(N, L);
     }
     "
 };
@@ -37,6 +42,8 @@ pub static FRAGMENT_SRC: ShaderSource<'static> = shaders! {
 pub struct Params {
     #[name = "u_Transform"]
     pub mvp: [[f32; 4]; 4],
+    #[name = "u_NormalRotation"]
+    pub normal: [[f32; 3]; 3],
     #[name = "u_Color"]
     pub color: [f32; 4],
 }
@@ -45,6 +52,7 @@ impl Params {
     pub fn new() -> Params {
         Params {
             mvp: [[0.0; 4]; 4],
+            normal: [[0.0; 3]; 3],
             color: [1.0, 1.0, 1.0, 1.0],
         }
     }
@@ -54,8 +62,9 @@ impl ::scene::ShaderParam<Scalar> for Params {
     fn set_transform(&mut self,
                      camera: &cgmath::Matrix4<Scalar>,
                      model: &cgmath::Matrix4<Scalar>,
-                     _view: &::scene::Transform<Scalar>) {
-        use cgmath::{Matrix, FixedArray};
+                     view: &::scene::Transform<Scalar>) {
+        use cgmath::{Matrix, ToMatrix3, FixedArray};
         self.mvp = camera.mul_m(model).into_fixed();
+        self.normal = view.rot.to_matrix3().into_fixed();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,15 @@ use glfw::Context;
 
 fn main() {
     println!("Initializing the window...");
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS)
-        .ok().expect("Failed to initialize glfs-rs");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 2));
     glfw.window_hint(glfw::WindowHint::OpenglForwardCompat(true));
     glfw.window_hint(glfw::WindowHint::OpenglProfile(glfw::OpenGlProfileHint::Core));
 
     let (mut window, events) = glfw
-        .create_window(640, 480, "Cube example", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window.");
+        .create_window(640, 480, "Claymore", glfw::WindowMode::Windowed)
+        .unwrap();
 
     window.make_current();
     glfw.set_error_callback(glfw::FAIL_ON_ERRORS);
