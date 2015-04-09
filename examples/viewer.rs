3f7a437edea57ff3e293c2fa75740910060ad425
Dzmitry Malyshau <kvarkus@gmail.com>
2015-04-08 22:38:37 -0400

    Implemented the camera control for the viewer

--- examples/viewer.rs
+++ examples/viewer.rs
@@ -8,6 +8,70 @@ extern crate gfx_debug_draw;
 extern crate claymore_load;
 extern crate claymore_scene; //temp
 
+type MousePos = (i32, i32);
+
+struct Control {
+    rotate_speed: f32,
+    zoom_speed: f32,
+    base: Option<(MousePos, claymore_scene::Transform<f32>)>,
+    last_pos: MousePos,
+    space: claymore_scene::Transform<f32>,
+}
+
+impl Control {
+    pub fn new(rot_speed: f32, zoom_speed: f32,
+               space: claymore_scene::Transform<f32>) -> Control {
+        Control {
+            rotate_speed: rot_speed,
+            zoom_speed: zoom_speed,
+            base: None,
+            last_pos: (0, 0),
+            space: space,
+        }
+    }
+
+    pub fn capture(&mut self, transform: &claymore_scene::Transform<f32>) {
+        self.base = Some((self.last_pos, transform.clone()));
+    }
+
+    pub fn release(&mut self) {
+        self.base = None;
+    }
+
+    pub fn position(&mut self, coords: MousePos,
+                    transform: &mut claymore_scene::Transform<f32>) {
+        self.last_pos = coords;
+        match self.base {
+            Some((ref base_pos, ref base_transform)) => {
+                use cgmath::Transform;
+                // p' = Mp * Tc^ * (Tr * Rz * Tr^) * p
+                // Tx = (Tr * Rz^ * Tr^) * Tc
+                let path = (coords.0 - base_pos.0) as f32 * -self.rotate_speed;
+                let rotation = cgmath::Decomposed {
+                    scale: 1.0,
+                    rot: cgmath::Rotation3::from_axis_angle(
+                        &cgmath::vec3(0.0, 0.0, 1.0), cgmath::rad(path)),
+                    disp: cgmath::zero(),
+                };
+                let space_inv = self.space.invert().unwrap();
+                let relative = self.space.concat(&rotation.concat(&space_inv));
+                *transform = relative.concat(base_transform);
+            },
+            None => (),
+        }
+    }
+
+    pub fn wheel(&mut self, shift: i32, transform: &mut claymore_scene::Transform<f32>) {
+        use cgmath::{Vector, Transform};
+        if self.base.is_some() {
+            return
+        }
+        let vector = transform.transform_vector(&cgmath::vec3(0.0, 0.0, 1.0));
+        transform.disp.add_self_v(&vector.mul_s(shift as f32 * -self.zoom_speed));
+    }
+}
+
+
 fn main() {
     use std::env;
     use cgmath::{vec3, FixedArray, Matrix, ToMatrix4, Transform};
@@ -52,6 +116,14 @@ fn main() {
     pipeline.background = Some([0.2, 0.3, 0.4, 1.0]);
     let mut frame = gfx::Frame::new(w as u16, h as u16);
 
+    let mut camera = scene.cameras[0].clone();
+    camera.projection.aspect = w as f32 / h as f32;
+    let mut control = {
+        use claymore_scene::base::World;
+        let target_node = scene.entities[0].node;
+        Control::new(0.005, 0.5, scene.world.get_transform(&target_node))
+    };
+
     println!("Rendering...");
     'main: loop {
         for event in window.poll_events() {
@@ -65,19 +137,26 @@ fn main() {
                     Some(glutin::VirtualKeyCode::Escape)) =>
                     break 'main,
                 glutin::Event::Closed => break 'main,
+                glutin::Event::MouseInput(glutin::ElementState::Pressed, glutin::MouseButton::Left) =>
+                    control.capture(&scene.world.get_node(camera.node).local),
+                glutin::Event::MouseInput(glutin::ElementState::Released, glutin::MouseButton::Left) =>
+                    control.release(),
+                glutin::Event::MouseMoved(coords) =>
+                    control.position(coords, &mut scene.world.mut_node(camera.node).local),
+                glutin::Event::MouseWheel(shift) =>
+                    control.wheel(shift, &mut scene.world.mut_node(camera.node).local),
                 _ => {},
             }
         }
 
         scene.world.update();
-        let mut camera = scene.cameras[0].clone();
-        camera.projection.aspect = w as f32 / h as f32;
+        let len = 1f32;
 
         for node in scene.world.iter_nodes() {
             let r = node.world.transform_vector(&vec3(0.0, 0.0, 0.0)).into_fixed();
-            let x = node.world.transform_vector(&vec3(1.0, 0.0, 0.0)).into_fixed();
-            let y = node.world.transform_vector(&vec3(0.0, 1.0, 0.0)).into_fixed();
-            let z = node.world.transform_vector(&vec3(0.0, 0.0, 1.0)).into_fixed();
+            let x = node.world.transform_vector(&vec3(len, 0.0, 0.0)).into_fixed();
+            let y = node.world.transform_vector(&vec3(0.0, len, 0.0)).into_fixed();
+            let z = node.world.transform_vector(&vec3(0.0, 0.0, len)).into_fixed();
             debug_renderer.draw_line(r, x, [1.0, 0.0, 0.0, 1.0]);
             debug_renderer.draw_line(r, y, [0.0, 1.0, 0.0, 1.0]);
             debug_renderer.draw_line(r, z, [0.0, 0.0, 1.0, 1.0]);
@@ -88,7 +167,7 @@ fn main() {
 
         //debug_renderer.render(&mut graphics, &frame,
         //    camera.get_view_projection(&scene.world));
-        {
+        if true {
             use cgmath::FixedArray;
             use claymore_scene::base::World;
             let cam_inv = scene.world.get_transform(&camera.node).invert().unwrap();
