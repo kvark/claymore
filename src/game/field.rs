4355ab525b5af2fa129ace8096a7eea2d690acbf
Dzmitry Malyshau <kvarkus@gmail.com>
2015-04-27 23:53:05 -0400

    cleaned up field shaders

--- src/game/field.rs
+++ src/game/field.rs
@@ -27,7 +27,6 @@ impl Vertex {
     }
 }
 
-#[cfg(not(target_os = "macos"))]
 static VERTEX_SRC: &'static [u8] = b"
     #version 150 core
 
@@ -40,20 +39,6 @@ static VERTEX_SRC: &'static [u8] = b"
     }
 ";
 
-#[cfg(target_os = "macos")]
-static VERTEX_SRC: &'static [u8] = b"
-    #version 150
-
-    uniform mat4 mvp;
-    in vec2 position;
-
-    void main() {
-        gl_Position = mvp * vec4(position, 0.0, 1.0);
-        gl_PointSize = 2.0;
-    }
-";
-
-#[cfg(not(target_os = "macos"))]
 static FRAGMENT_SRC: &'static [u8] = b"
     #version 150 core
 
@@ -64,19 +49,6 @@ static FRAGMENT_SRC: &'static [u8] = b"
         o_Color = color;
     }
 ";
-
-#[cfg(target_os = "macos")]
-static FRAGMENT_SRC: &'static [u8] = b"
-    #version 150
-
-    uniform vec4 color;
-    out vec4 f_color;
-
-    void main() {
-        f_color = color;
-    }
-";
-
 
 pub use grid::quad::{Coordinate, Direction};
 
