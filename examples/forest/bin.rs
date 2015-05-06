3ef2437000bc531c10df74fefe3760a9758d9358
Dzmitry Malyshau <kvarkus@gmail.com>
2015-05-05 22:25:38 -0400

    Moved the generator algorithm into a separate module

--- examples/forest/bin.rs
+++ examples/forest/bin.rs
@@ -12,28 +12,9 @@ extern crate gfx_debug_draw;
 extern crate claymore_load;
 extern crate claymore_scene;
 
+mod generate;
 mod reflect;
 
-
-struct TileInfo<R: gfx::Resources> {
-    node: claymore_scene::NodeId<f32>,
-    mesh: gfx::Mesh<R>,
-    fragments: Vec<claymore_scene::Fragment<R>>,
-    river_mask: u8,
-}
-
-impl<R: gfx::Resources> TileInfo<R> {
-    pub fn fit_orientation(&self, neighbors: u8, rivers: u8) -> Option<u8> {
-        let mut mask = self.river_mask;
-        for i in 0u8.. 4 {
-            if (mask & neighbors) == rivers {
-                return Some(i)
-            }
-            mask = ((mask << 1) & 0xF) | (mask >> 3);
-        }
-        None
-    }
-}
 
 fn move_camera<S: cgmath::BaseFloat>(
     camera: &claymore_scene::Camera<S>,
@@ -84,135 +65,9 @@ fn main() {
                                            .unwrap();
     scene.world.update();
 
-    println!("Processing data...");
-    let tile_info: Vec<_> = config.palette.tiles.iter().map(|t| {
-        let mask = t.river.chars().fold(0, |m, c| match c {
-            'n' => m | 1,
-            'e' => m | 2,
-            's' => m | 4,
-            'w' => m | 8,
-            _   => panic!("Unknown river direction: {}", c),
-        });
-        let ent = scene.entities.iter()
-                                .find(|ent| ent.name == t.name)
-                                .expect(&format!("Unable to find entity {}", t.name));
-        info!("Found tile {} with river mask {}",
-            t.name, mask);
-        TileInfo {
-            node: ent.node.clone(),
-            mesh: ent.mesh.clone(),
-            fragments: ent.fragments.clone(),
-            river_mask: mask,
-        }
-    }).collect();
-
-    println!("Generating content...");
     if config.generate {
-        use std::collections::HashMap;
-        type Position = (i32, i32);
-        struct Tile {
-            info_id: usize,
-            orientation: u8,
-        }
-        scene.entities.clear();
-        let mut rng = rand::thread_rng();
-        let mut tile_map: HashMap<Position, Tile> = HashMap::new();
-        for y in -config.size.0 ..config.size.0 {
-            for x in -config.size.1 ..config.size.1 {
-                use rand::Rng;
-                if tile_map.contains_key(&(x,y)) {
-                    continue
-                }
-                debug!("Generating tile {:?}", (x,y));
-                // figure out what neighbour edges are rivers
-                let mut river_mask = 0;
-                let mut neighbour_mask = 0;
-                let offsets = [[0, 1], [1, 0], [0,-1], [-1, 0]];
-                for (bit, off) in offsets.iter().enumerate() {
-                    let pos = (x + off[0], y + off[1]);
-                    if let Some(tile) = tile_map.get(&pos) {
-                        neighbour_mask |= 1 << bit;
-                        let river_bit = ((bit as u8) + 6 - tile.orientation) & 3;
-                            debug!("\tChecking for river bit {} of neighbor dir {}", river_bit, bit);
-                        let info = &tile_info[tile.info_id];
-                        if info.river_mask & (1 << river_bit) != 0 {
-                            river_mask |= 1 << bit;
-                        }
-                    }
-                }
-                debug!("\tLooking for river mask {} of neighbors {}", river_mask, neighbour_mask);
-                // find a matching prototype
-                let mut matched = 0;
-                for info in tile_info.iter() {
-                    if info.fit_orientation(neighbour_mask, river_mask).is_some() {
-                        matched += 1;
-                    }
-                }
-                if matched == 0 {
-                    error!("Couldn't find a tile match for {:?}, where neighbors = {}, rivers = {}",
-                        (x, y), neighbour_mask, river_mask);
-                    continue
-                }
-                let chosen = rng.gen_range(0, matched);
-                debug!("\tChosen match {} of total {}", chosen, matched);
-                matched = 0;
-                for (id, info) in tile_info.iter().enumerate() {
-                    match info.fit_orientation(neighbour_mask, river_mask) {
-                        Some(orientation) if matched == chosen => {
-                            use cgmath::ToRad;
-                            debug!("\tUsing orientation {} and info id {}", orientation, id);
-                            let size = config.palette.size;
-                            let rotation = {
-                                use cgmath::Rotation;
-                                use claymore_scene::base::World;
-                                let relative: cgmath::Quaternion<_> = cgmath::Rotation3::from_axis_angle(
-                                    &cgmath::Vector3::new(0.0, 0.0, -1.0),
-                                    cgmath::deg(orientation as f32 * 90.0).to_rad(),
-                                );
-                                let node = tile_info[id].node;
-                                relative.concat(&scene.world.get_transform(&node).rot)
-                            };
-                            let (rot_x, rot_y) = [(0, 0), (0, 1), (1, 1), (1, 0)][orientation as usize];
-                            let node = scene.world.add_node(
-                                format!("Tile ({}, {})", x, y),
-                                claymore_scene::space::Parent::None,
-                                cgmath::Decomposed {
-                                    scale: 1.0,
-                                    rot: rotation,
-                                    disp: cgmath::Vector3::new(
-                                        (x + rot_x) as f32 * size,
-                                        (y + rot_y) as f32 * size,
-                                        0.0,
-                                    ),
-                                });
-                            // add the new entity to the scene
-                            scene.entities.push(claymore_scene::base::Entity {
-                                name: String::new(),
-                                visible: true,
-                                mesh: tile_info[id].mesh.clone(),
-                                node: node,
-                                skeleton: None,
-                                bound: cgmath::Aabb3::new(
-                                    cgmath::Point3::new(0.0, 0.0, 0.0),
-                                    cgmath::Point3::new(size, 0.5, -size),
-                                ),
-                                fragments: tile_info[id].fragments.clone(),
-                            });
-                            // register the new tile
-                            tile_map.insert((x, y), Tile {
-                                info_id: id,
-                                orientation: orientation,
-                            });
-                            break;
-                        }
-                        Some(_) => {
-                            matched += 1;
-                        }
-                        None => (),
-                    }
-                }
-            }
-        }
+        let gen = generate::Gen::new(&config.palette, &scene);
+        gen.populate(config.size, &mut scene);
     }
 
     println!("Initializing the graphics...");
--- examples/forest/generate.rs
+++ examples/forest/generate.rs
@@ -1,3 +1,7 @@
+use rand;
+use cgmath;
+use gfx;
+use gfx_pipeline::Material;
 use claymore_scene;
 use reflect;
 
@@ -5,16 +9,18 @@ use reflect;
 struct TileComponent<R: gfx::Resources> {
     mesh: gfx::Mesh<R>,
     slice: gfx::Slice<R>,
-    material: gfx_pipeline::Material<R>,
+    material: Material<R>,
 }
 
-struct TipeProto<R: gfx::Resources> {
+struct TileProto<R: gfx::Resources> {
     node: claymore_scene::NodeId<f32>,
-    components: Vec<TileComponent<R>>,
+    mesh: gfx::Mesh<R>,
+    fragments: Vec<claymore_scene::Fragment<R>>,
     river_mask: u8,
+
 }
 
-impl<R: gfx::Resources> TileInfo<R> {
+impl<R: gfx::Resources> TileProto<R> {
     pub fn fit_orientation(&self, neighbors: u8, rivers: u8) -> Option<u8> {
         let mut mask = self.river_mask;
         for i in 0u8.. 4 {
@@ -29,23 +35,13 @@ impl<R: gfx::Resources> TileInfo<R> {
 
 pub struct Gen<R: gfx::Resources> {
     proto_tiles: Vec<TileProto<R>>,
+    tile_size: f32,
 }
 
 impl<R: gfx::Resources> Gen<R> {
-    pub fn new(scene: &claymore_scene::Scene<R>) -> Gen<R> {
-        let config: reflect::Demo = {
-            use std::fs::File;
-            use std::io::Read;
-            use rustc_serialize::json;
-            let mut file = File::open(&format!("{}/examples/forest/config.json", root))
-                                .unwrap();
-            let mut s = String::new();
-            file.read_to_string(&mut s).unwrap();
-            json::decode(&s).unwrap()
-        };
-
+    pub fn new(config: &::reflect::Palette, scene: &claymore_scene::Scene<R, f32>) -> Gen<R> {
         println!("Processing data...");
-        let protos: Vec<_> = config.palette.tiles.iter().map(|t| {
+        let protos: Vec<_> = config.tiles.iter().map(|t| {
             let mask = t.river.chars().fold(0, |m, c| match c {
                 'n' => m | 1,
                 'e' => m | 2,
@@ -53,30 +49,132 @@ impl<R: gfx::Resources> Gen<R> {
                 'w' => m | 8,
                 _   => panic!("Unknown river direction: {}", c),
             });
-            let node = scene.entities.iter()    //TODO
-                                     .find(|ent| ent.name == t.name)
-                                     .map(|ent| ent.node.clone())
-                                     .unwrap();
-            let components: Vec<_> = scene.entities.iter()
-                                                   .filter(|ent| ent.name == t.name)
-                                                   .map(|ent|
-                TileComponent {
-                    mesh: ent.mesh.clone(),
-                    slice: ent.slice.clone(),
-                    material: ent.material.clone(),
-                }
-            ).collect();
-            info!("Found tile {} with {} components and river mask {}",
-                t.name, components.len(), mask);
+            let ent = scene.entities.iter()
+                                    .find(|ent| ent.name == t.name)
+                                    .expect(&format!("Unable to find entity {}", t.name));
+            info!("Found tile {} with river mask {}",
+                t.name, mask);
             TileProto {
-                node: node,
-                components: components,
+                node: ent.node.clone(),
+                mesh: ent.mesh.clone(),
+                fragments: ent.fragments.clone(),
                 river_mask: mask,
             }
         }).collect();
 
         Gen {
             proto_tiles: protos,
+            tile_size: config.size,
+        }
+    }
+
+    pub fn populate(&self, grid_size: (i32, i32),
+                    scene: &mut claymore_scene::Scene<R, f32>) {
+        use std::collections::HashMap;
+        type Position = (i32, i32);
+        struct Tile {
+            proto_id: usize,
+            orientation: u8,
+        }
+        scene.entities.clear();
+        println!("Generating content...");
+        let mut rng = rand::thread_rng();
+        let mut tile_map: HashMap<Position, Tile> = HashMap::new();
+        for y in -grid_size.0 ..grid_size.0 {
+            for x in -grid_size.1 ..grid_size.1 {
+                use rand::Rng;
+                if tile_map.contains_key(&(x,y)) {
+                    continue
+                }
+                debug!("Generating tile {:?}", (x,y));
+                // figure out what neighbour edges are rivers
+                let mut river_mask = 0;
+                let mut neighbour_mask = 0;
+                let offsets = [[0, 1], [1, 0], [0,-1], [-1, 0]];
+                for (bit, off) in offsets.iter().enumerate() {
+                    let pos = (x + off[0], y + off[1]);
+                    if let Some(tile) = tile_map.get(&pos) {
+                        neighbour_mask |= 1 << bit;
+                        let river_bit = ((bit as u8) + 6 - tile.orientation) & 3;
+                            debug!("\tChecking for river bit {} of neighbor dir {}", river_bit, bit);
+                        let proto = &self.proto_tiles[tile.proto_id];
+                        if proto.river_mask & (1 << river_bit) != 0 {
+                            river_mask |= 1 << bit;
+                        }
+                    }
+                }
+                debug!("\tLooking for river mask {} of neighbors {}", river_mask, neighbour_mask);
+                // find a matching prototype
+                let mut matched = 0;
+                for proto in self.proto_tiles.iter() {
+                    if proto.fit_orientation(neighbour_mask, river_mask).is_some() {
+                        matched += 1;
+                    }
+                }
+                if matched == 0 {
+                    error!("Couldn't find a tile match for {:?}, where neighbors = {}, rivers = {}",
+                        (x, y), neighbour_mask, river_mask);
+                    continue
+                }
+                let chosen = rng.gen_range(0, matched);
+                debug!("\tChosen match {} of total {}", chosen, matched);
+                matched = 0;
+                for (id, proto) in self.proto_tiles.iter().enumerate() {
+                    match proto.fit_orientation(neighbour_mask, river_mask) {
+                        Some(orientation) if matched == chosen => {
+                            use cgmath::ToRad;
+                            debug!("\tUsing orientation {} and proto id {}", orientation, id);
+                            let rotation = {
+                                use cgmath::Rotation;
+                                use claymore_scene::base::World;
+                                let relative: cgmath::Quaternion<_> = cgmath::Rotation3::from_axis_angle(
+                                    &cgmath::Vector3::new(0.0, 0.0, -1.0),
+                                    cgmath::deg(orientation as f32 * 90.0).to_rad(),
+                                );
+                                let node = self.proto_tiles[id].node;
+                                relative.concat(&scene.world.get_transform(&node).rot)
+                            };
+                            let (rot_x, rot_y) = [(0, 0), (0, 1), (1, 1), (1, 0)][orientation as usize];
+                            let node = scene.world.add_node(
+                                format!("Tile ({}, {})", x, y),
+                                claymore_scene::space::Parent::None,
+                                cgmath::Decomposed {
+                                    scale: 1.0,
+                                    rot: rotation,
+                                    disp: cgmath::Vector3::new(
+                                        (x + rot_x) as f32 * self.tile_size,
+                                        (y + rot_y) as f32 * self.tile_size,
+                                        0.0,
+                                    ),
+                                });
+                            // add the new entity to the scene
+                            let proto = &self.proto_tiles[id];
+                            scene.entities.push(claymore_scene::base::Entity {
+                                name: String::new(),
+                                visible: true,
+                                mesh: proto.mesh.clone(),
+                                node: node,
+                                skeleton: None,
+                                bound: cgmath::Aabb3::new(
+                                    cgmath::Point3::new(0.0, 0.0, 0.0),
+                                    cgmath::Point3::new(self.tile_size, 0.5, -self.tile_size),
+                                ),
+                                fragments: proto.fragments.clone(),
+                            });
+                            // register the new tile
+                            tile_map.insert((x, y), Tile {
+                                proto_id: id,
+                                orientation: orientation,
+                            });
+                            break;
+                        }
+                        Some(_) => {
+                            matched += 1;
+                        }
+                        None => (),
+                    }
+                }
+            }
         }
     }
 }
